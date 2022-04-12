6c9340853a633ee8df69d9da4e1731bdc35dba19
lyoha poplil <lyopa70ukr.net@gmail.com>
2022-04-12 13:48:30 +0300

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const checkForMe = id => id === process.env.USER_ID;
+
+bot.start( ctx => ctx.reply("Доброго вечора, ми з України"));
+
+bot.on("text", ctx => {
+    if (!checkForMe(ctx.update.message.from.id)) return;
+    console.log(ctx.update.message)
+    ctx.reply("пігнали досить")
+})
+
+bot.hears('check', (ctx) => {
+    ps.lookup({ command: './db1000n' }, (err, resultList ) => {
+        if (err) return console.log(err);
+
+        const process = resultList[0];
+        if (process) {
+            ctx.reply('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+        } else {
+            ctx.reply('No such process found!')
+        }
+    });
+})
+
+bot.launch();
