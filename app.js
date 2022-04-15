a2db05335b61004c04cc604e4fc93ebc27d1e8dd
lyoha poplil <lyopa70ukr.net@gmail.com>
2022-04-15 11:56:46 +0300

    update

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const { Telegraf } = require('telegraf');
+const shell = require('shelljs');
+const ps = require('ps-node');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const checkForMe = id => id === process.env.USER_ID;
+const checkForMe = id => id === +process.env.USER_ID;
+
+const checkProcess = async (command) => {
+    return new Promise((res, rej) => {
+        ps.lookup({ command }, (err, resultList ) => {
+            if (err) rej(err);
+            if (resultList[0]) {
+                res(resultList[0].command)
+            } else {
+                res()
+            }
+        });
+    })
+}
 
 bot.start( ctx => ctx.reply("Доброго вечора, ми з України"));
 
-bot.on("text", ctx => {
+bot.hears('check', async (ctx) => {
     if (!checkForMe(ctx.update.message.from.id)) return;
-    console.log(ctx.update.message)
-    ctx.reply("пігнали досить")
+    const aliveProces = [];
+    for (const command of process.env.PROCESSES_FIND.split(" ")) {
+        const process = await checkProcess(command);
+        if (process) aliveProces.push(process);
+    }
+    if (aliveProces.length === 0) {
+        ctx.reply("no alive process");
+        return
+    }
+    ctx.reply(aliveProces.join(" "));
 })
 
-bot.hears('check', (ctx) => {
-    ps.lookup({ command: './db1000n' }, (err, resultList ) => {
-        if (err) return console.log(err);
-
-        const process = resultList[0];
-        if (process) {
-            ctx.reply('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
-        } else {
-            ctx.reply('No such process found!')
-        }
-    });
+bot.hears('exec', async ctx => {
+    for (const comand of process.env.PROCESSES_START.split(" ")) {
+        shell.exec(`../comand`, {async:true});
+    }
 })
 
 bot.launch();
